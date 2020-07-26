f6c1bc1ecde0d93c6add8f9132888036b6339a55
vonGradnell <47977176+vonGradnell@users.noreply.github.com>
2020-07-26 18:17:33 +0300

    Add files via upload

---
+++ ch1/ch1_21.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+    Sales_item item1, item2;
+    std::cout << "Enter 2 item: " << std::endl;
+    std::cin >> item1 >> item2;
+    std::cout << item1 + item2 << std::endl;
+}
